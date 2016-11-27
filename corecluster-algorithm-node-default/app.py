4e961a16d4de268b0596f6ac49250171181a283c
Maciej Nabozny <maciej.nabozny@cloudover.io>
2016-11-27 10:39:33 +0100

    app.py

--- corecluster-algorithm-node-default/app.py
+++ corecluster-algorithm-node-default/app.py
@@ -1,5 +1,5 @@
 MODULE = {
     'algorithms': {
-        'NODE_SELECT': 'corecluster-algorithm-node-default.algorithms/default.py',
+        'NODE_SELECT': 'corecluster-algorithm-node-default.algorithms.default',
     },
 }
